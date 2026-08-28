e920281e37dc0470816678450e1144ad64ca102a
Chandra Vardhan <chandravardhanpeddineni@gmail.com>
2026-08-28 12:42:24 +0530

    tailwind + daisy UI + navabar updated

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  // DaisyUI will be configured via tailwind.config.cjs (Vite/Tailwind v4 build pipeline).
+  plugins: [],
+}
+
+ 
+
